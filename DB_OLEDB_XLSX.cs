239d0ac444fa824fff13bbaa2c1a252294f137d2
thines01 <thines01@yahoo.com>
2023-07-09 09:46:24 -0500

    Add project files.

---
+++ DB_OLEDB_XLSX.cs
@@ -0,0 +1,32 @@
+﻿using System.Data.OleDb;
+using System.IO;
+
+namespace DB_OLEDB_XLSX
+{
+   public partial class CDB_OLEDB_XLSX
+   {
+      public OleDbConnectionStringBuilder csb { get; set; }
+
+      private static OleDbConnectionStringBuilder GetCsFromFi(FileInfo fi, bool blnHeader)
+      {
+         string strHasHeader = blnHeader ? "Yes" : "No";
+
+         return new OleDbConnectionStringBuilder()
+         {
+            ConnectionString = "Extended Properties=\"Excel 12.0 Xml;HDR=" + strHasHeader + ";IMEX=1;MAXSCANROWS=2;READONLY=TRUE\";",
+            DataSource = fi.FullName,
+            Provider = "Microsoft.ACE.OLEDB.12.0",
+         };
+      }
+
+      public CDB_OLEDB_XLSX(FileInfo fi, bool blnHeader = true)
+      {
+         csb = GetCsFromFi(fi, blnHeader);
+      }
+
+      public CDB_OLEDB_XLSX(string strFileName, bool blnHeader = true)
+      {
+         csb = GetCsFromFi(new FileInfo(strFileName), blnHeader);
+      }
+   }
+}
---
+++ GetSheet1Name.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Linq;
+
+namespace DB_OLEDB_XLSX
+{
+   public partial class CDB_OLEDB_XLSX
+   {
+      public static readonly Func<OleDbConnection, string> GetSheet1Name = (conn) =>
+         conn
+         .GetOleDbSchemaTable(OleDbSchemaGuid.Tables_Info, new[] { null, null, null, "TABLE" })
+         .Select().Select(row => row["TABLE_NAME"]).FirstOrDefault().ToString();
+   }
+}
